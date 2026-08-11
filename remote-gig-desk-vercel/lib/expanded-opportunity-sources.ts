90eedb382081abe24a61d16cccd0a3b8b0ae7a91
crz0614 <chenruozhu0614@gmail.com>
2026-08-11 16:33:07 +0800

    Fix escaped postings and restore AI generation

--- remote-gig-desk-vercel/lib/expanded-opportunity-sources.ts
+++ remote-gig-desk-vercel/lib/expanded-opportunity-sources.ts
@@ -31,7 +31,7 @@ const defaultCareers: CareerSource[] = [
 ];
 
 function text(value = "") {
-  return value.replace(/<br\s*\/?\s*>|<\/(?:p|div|li|h[1-6])>/gi, "\n")
+  return value.replace(/&lt;/gi,"<").replace(/&gt;/gi,">").replace(/&quot;/gi,'"').replace(/&#39;|&apos;/gi,"'").replace(/&nbsp;/gi," ").replace(/&amp;/gi,"&").replace(/&#(\d+);/g,(_,code)=>String.fromCodePoint(Number(code))).replace(/&#x([0-9a-f]+);/gi,(_,code)=>String.fromCodePoint(parseInt(code,16))).replace(/<br\s*\/?\s*>|<\/(?:p|div|li|h[1-6])>/gi, "\n")
     .replace(/<li[^>]*>/gi, "• ").replace(/<[^>]+>/g, " ")
     .replace(/&nbsp;/gi, " ").replace(/&amp;/gi, "&").replace(/&quot;/gi, '"')
     .replace(/&#39;|&apos;/gi, "'").replace(/[ \t]+/g, " ").replace(/\n{3,}/g, "\n\n").trim();
